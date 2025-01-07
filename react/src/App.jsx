0274bd4ffd1558dc4978622e17f7e22f373759f7
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-01-07 20:58:56 +0700

    react components

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,11 @@
 function App() {
   const name = "Jaenudin Firdaus";
-  return <Hello name={name} />;
+  return (
+    <>
+      <Hello name={name} />
+      <AnotherComponent />
+    </>
+  );
 }
 
 function Hello({ name }) {
@@ -12,6 +17,10 @@ function Hello({ name }) {
       Hello <span style={styling}>{name}</span>, How are you doing?
     </h1>
   );
+}
+
+function AnotherComponent() {
+  return <h2>Another Component</h2>;
 }
 
 export default App;
