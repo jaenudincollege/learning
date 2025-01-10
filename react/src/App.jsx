80f0f4c24e28527746dc6550ec02f5bdb13f8c11
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-01-10 22:00:38 +0700

    function props

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,17 +1,40 @@
-function Button({ text, color, fontSize }) {
+function Button({ text, color, fontSize, handleClick }) {
   const btnStyle = {
     color,
     fontSize,
   };
-  return <button style={btnStyle}>{text}</button>;
+  return (
+    <button style={btnStyle} onClick={handleClick}>
+      {text}
+    </button>
+  );
 }
 
 export default function App() {
+  function buttonHandleClick() {
+    window.location.href = "https://google.com";
+  }
+
   return (
     <>
-      <Button text="Click Me!" color="red" fontSize={12} />
-      <Button text="Click Me!" color="yellow" fontSize={16} />
-      <Button text="Click Me!" color="cyan" fontSize={24} />
+      <Button
+        text="Click Me!"
+        color="red"
+        fontSize={12}
+        handleClick={buttonHandleClick}
+      />
+      <Button
+        text="Click Me!"
+        color="yellow"
+        fontSize={16}
+        handleClick={buttonHandleClick}
+      />
+      <Button
+        text="Click Me!"
+        color="cyan"
+        fontSize={24}
+        handleClick={buttonHandleClick}
+      />
     </>
   );
 }
