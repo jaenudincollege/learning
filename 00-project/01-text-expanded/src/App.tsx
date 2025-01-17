b58672102df96882fbb8c498c1e8cff18180777b
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-01-17 17:41:51 +0700

    using TextExpander component

--- 00-project/01-text-expanded/src/App.tsx
+++ 00-project/01-text-expanded/src/App.tsx
@@ -1,3 +1,14 @@
+import TextExpander from "./components/TextExpander";
+
 export default function App() {
-  return "any";
+  return (
+    <div className="container">
+      <TextExpander>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dicta
+        aliquid itaque sed perferendis excepturi reiciendis libero
+        exercitationem. Ipsam minima illum voluptate, ea labore eos rerum libero
+        voluptates ipsum vitae.
+      </TextExpander>
+    </div>
+  );
 }
