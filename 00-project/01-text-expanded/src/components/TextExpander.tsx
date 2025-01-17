7ae6d67469c1aee84fc97bc01e71bca2d1aeffec
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-01-17 17:51:32 +0700

    create styling object for button element

--- 00-project/01-text-expanded/src/components/TextExpander.tsx
+++ 00-project/01-text-expanded/src/components/TextExpander.tsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 
-export default function TextExpander({ children }) {
+export default function TextExpander({ children, btnColor = "red" }) {
   const [isOpen, setIsOpen] = useState(false);
+
+  const view = isOpen
+    ? children
+    : children.split(" ").slice(0, 20).join(" ") + " ...";
+
+  const btnStyle = {
+    fontSize: "2.4rem",
+    border: "none",
+    background: "none",
+    marginLeft: "2rem",
+    cursor: "pointer",
+    color: btnColor,
+  };
 
   return (
     <div className="card">
-      <span className="text">{children}</span>
-      <button className="btn">Show more</button>
+      <span className="text">{view}</span>
+      <button style={btnStyle} onClick={() => setIsOpen((open) => !open)}>
+        Show more
+      </button>
     </div>
   );
 }
