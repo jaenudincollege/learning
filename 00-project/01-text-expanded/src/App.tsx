126428860ae661dd30c47330921bc69908ee2eac
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-01-17 17:57:54 +0700

    testing the result

--- 00-project/01-text-expanded/src/App.tsx
+++ 00-project/01-text-expanded/src/App.tsx
@@ -9,6 +9,27 @@ export default function App() {
         exercitationem. Ipsam minima illum voluptate, ea labore eos rerum libero
         voluptates ipsum vitae.
       </TextExpander>
+
+      <TextExpander
+        expanded={true}
+        className="card"
+        btnColor="#748ffc"
+        btnOpen="Read more"
+        btnClose="Read less"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa autem
+        labore inventore deleniti! Sit dignissimos natus quae ex alias esse
+        sapiente quod, incidunt recusandae debitis architecto qui animi, neque
+        nulla voluptatem placeat vel perspiciatis in sunt doloribus voluptates!
+        Voluptas voluptate amet libero odio! In unde nihil officia doloribus
+        provident veritatis harum eveniet quaerat. Optio aspernatur hic totam,
+        perspiciatis illum, nulla asperiores debitis perferendis cupiditate sunt
+        eveniet repudiandae deserunt nobis consequatur, quo cumque ipsa id
+        repellendus est praesentium blanditiis laborum minima! Nulla mollitia
+        quos dignissimos laboriosam voluptas illo provident maiores a
+        praesentium, voluptatem fugit eveniet perspiciatis laborum pariatur ipsa
+        officia facere!
+      </TextExpander>
     </div>
   );
 }
